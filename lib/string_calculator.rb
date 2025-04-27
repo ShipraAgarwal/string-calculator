7eb0e072d35d76dff8a529b6ae104ade068d592d
Shipra Agarwal <shipraa137@gmail.com>
2025-04-27 20:10:30 +0530

    step 4 - handle different delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,13 @@
 class StringCalculator
-  def add(str_of_numbers)
+  def add(str_of_numbers:, delimiter: ',')
     return 0 if str_of_numbers.empty?
 
-    return str_of_numbers.gsub('\n', ',').split(',').map(&:to_i).sum
+    if str_of_numbers.start_with?('//')
+      parts = str_of_numbers.split('\n', 2)
+      delimiter = parts[0][2]
+      str_of_numbers = parts[1]
+    end
+
+    return str_of_numbers.gsub('\n', delimiter).split(delimiter).map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,29 +4,37 @@ RSpec.describe StringCalculator do
   describe '#add' do
     context 'handle upto 2 comma separated numbers' do
       it 'returns 0 for empty string' do
-        expect(subject.add('')).to eq(0)
+        expect(subject.add(str_of_numbers: '')).to eq(0)
       end
 
       it 'returns the same number for a single number string' do
-        expect(subject.add('2')).to eq(2)
+        expect(subject.add(str_of_numbers: '2')).to eq(2)
       end
 
       it 'returns the sum of numbers for a two numbers string' do
-        expect(subject.add('2,3')).to eq(5)
+        expect(subject.add(str_of_numbers: '2,3')).to eq(5)
       end
     end
 
     context 'handle any amount of comma separated numbers' do
       it 'returns sum of all the numbers' do
-        expect(subject.add('2,3,4,5')).to eq(14)
+        expect(subject.add(str_of_numbers: '2,3,4,5')).to eq(14)
 
-        expect(subject.add('200, 5, 10, 15, 120')).to eq(350)
+        expect(subject.add(str_of_numbers: '200, 5, 10, 15, 120')).to eq(350)
       end
     end
 
     context 'handle new line as delimiter' do
       it 'returns sum of all the numbers' do
-        expect(subject.add('2\n3,4\n5')).to eq(14)
+        expect(subject.add(str_of_numbers: '2\n3,4\n5')).to eq(14)
+      end
+    end
+
+    context 'support different delimiters' do
+      it 'returns sum of all the numbers' do
+        expect(subject.add(str_of_numbers: '//;\n1;2')).to eq(3)
+
+        expect(subject.add(str_of_numbers: '//*\n1*2\n5')).to eq(8)
       end
     end
   end
